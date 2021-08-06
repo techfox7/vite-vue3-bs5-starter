34574b132bc962827473d0c3b880b02747227c6f
techfox <techfox7@outlook.com>
2021-08-06 14:41:58 +0900

    upgrade to bs 5.1.0

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -31,6 +31,29 @@
     </div>
   </div>
 
+  <div class="vstack gap-3">
+    <div class="bg-light border">First item</div>
+    <div class="bg-light border">Second item</div>
+    <div class="bg-light border">Third item</div>
+  </div>
+
+  <div class="card" aria-hidden="true">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title placeholder-glow">
+        <span class="placeholder col-6"></span>
+      </h5>
+      <p class="card-text placeholder-glow">
+        <span class="placeholder col-7"></span>
+        <span class="placeholder col-4"></span>
+        <span class="placeholder col-4"></span>
+        <span class="placeholder col-6"></span>
+        <span class="placeholder col-8"></span>
+      </p>
+      <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
+    </div>
+  </div>
+
   <!-- Button trigger modal -->
   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
     Launch demo modal
